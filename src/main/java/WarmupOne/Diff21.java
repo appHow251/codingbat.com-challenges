9a874f6114e4d0713196c1109e007e06ad761aef
April Howard <apehow111@gmail.com>
2021-03-22 10:41:45 -0400

    moved Math.abs to reflect purpose of challenge

--- src/main/java/WarmupOne/Diff21.java
+++ src/main/java/WarmupOne/Diff21.java
@@ -16,7 +16,7 @@ public class Diff21 {
         if(n > 21) {
             return Math.abs(n - 21) * 2;
         }
-        return Math.abs(21 -n);
+        return Math.abs(n-21);
     }
 }
 
--- src/test/java/WarmupOne/Diff21Test.java
+++ src/test/java/WarmupOne/Diff21Test.java
@@ -1,23 +1,18 @@
 package WarmupOne;
 
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
-
-import static org.junit.Assert.*;
 
 public class Diff21Test {
 
     private Diff21 diff21 = new Diff21();
 
+    @Test
+    public void diffWhenNGreaterThan21() {
+        Assert.assertEquals(4, diff21.diff21(23));
+    }
 
-        @Test
-        public void diffWhenNGreaterThan21() {
-            Assert.assertEquals(4, diff21.diff21(23));
-        }
-
-
-        @Test
+    @Test
     public void diffWhenNLessThan21() {
         Assert.assertEquals(4, diff21.diff21(17));
     }
